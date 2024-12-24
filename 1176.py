02ef445fd5346f518d268bee07d6ed7ca70f95ca
João Pedro Adam de Silva de Souza <aco12tgoficial@gmail.com>
2024-12-23 23:13:38 -0300

    Exercicio 1176 resolvido

--- 1176.py
+++ 1176.py
@@ -1,1 +1,16 @@
 #Resolver este problema
+
+def func_fib(num):
+    fib = [0,1]
+    for i in range(1,num):
+        fib.append(fib[i-1] + fib[i])
+    return fib[num]
+
+if __name__ == '__main__':
+    count = int(input())
+    n = []
+    for _ in range(count):
+        n.append(int(input()))
+    
+    for i in n:
+        print(f'Fib({i}) = {func_fib(i)}')
