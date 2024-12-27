aca72b70f77e1ab6e1a5e17c538c3ebc6f0002e0
João Pedro Adam de Silva de Souza <aco12tgoficial@gmail.com>
2024-12-27 14:46:41 -0300

    Exercício 1973 Resolvido

--- 1973.py
+++ 1973.py
@@ -1,31 +1,22 @@
-#Estrela 1 é o proprio sítio e rouba 1
-
-#se o sitio estrela i tem carneiro rouba 1
-
-# e segue ou para no sitio i + 1 se o numero de carneiros em estrela i é impar
-# ou volta para o sitio i - 1 se o numero de carneiros em estrela i é par
-
-#Se precisar avançar para a estrela i + 1 e ela não existir, então paramos o programa
-
 def calc_carneiros(vet_sitios):
-    soma = sum(vet_sitios)
     vet_aux = vet_sitios
-    i = 0
-    carneiros = 0
-    #Definindo o range que o i não pode passar de -N até N.
-    while i != -len(vet_sitios) - 1 and i != len(vet_sitios) - 1:
-        
-        if vet_aux[i] > 0:
-            carneiros += 1
-            vet_aux[i] -= 1
-        #impar
-        if vet_aux[i] % 2 == 1:
-            i += 1
-        #par
-        if vet_aux[i] % 2 == 0:
-            i -= 1
-        
-    print(f'{carneiros} {soma - carneiros}')
+    index = 0
+    sitios_atacados = 1
+
+    while True:
+        if index < 0 or index > len(vet_sitios) - 1:
+            break
+        atual = vet_aux[index]
+        if index + 1 > sitios_atacados: sitios_atacados = index + 1
+
+        if atual % 2 == 0: #par
+            if vet_aux[index] > 0: vet_aux[index] -= 1
+            index -= 1
+        elif atual % 2 == 1: #impar
+            if vet_aux[index] > 0: vet_aux[index] -= 1
+            index += 1
+            
+    print(f'{sitios_atacados} {sum(vet_aux)}')
 
 if __name__ == '__main__':
     N = int(input())
